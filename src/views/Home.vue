7f77bb4c5b6b3756d7b64450e29196152e6a6d67
wangyongtao <wangyongtao@yiche.com>
2022-06-17 14:30:19 +0800

    axios test.
    Change-Id: I71d9b35dbbd77bff068da4ffb53fb3d258dc1248

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="page">
         <h2>Home Page</h2>
+        <el-button type="primary" @click="testAxios()">测试Axios调用</el-button>
     </div>
 </template>
+
 <script>
+    import axios from 'axios'
+
     export default {
-        name: 'Home'
+        name: 'Home',
+        methods: {
+            testAxios() {
+                axios
+                    .get(`http://${window.location.host}`)
+                    .then(res => {
+                        alert(res.data)
+                    })
+            }
+        }
     }
 </script>
