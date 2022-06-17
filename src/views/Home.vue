fc522459f3a9ae241f2ef5c46626b3dd816f4dc0
wangyongtao <wangyongtao@yiche.com>
2022-06-17 15:10:41 +0800

    mock测试.
    Change-Id: Ie869f479839a14190cac2df80c6582eb5b4ed816

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,15 @@
     <div class="page">
         <h2>Home Page</h2>
         <el-button type="primary" @click="testAxios()">测试Axios调用</el-button>
+        <el-button type="primary" @click="getUser()">获取用户信息</el-button>
+        <el-button type="primary" @click="getMenu()">获取菜单信息</el-button>
     </div>
 </template>
 
 <script>
     import axios from 'axios'
+    // eslint-disable-next-line no-unused-vars
+    import mock from '../mock/mock.js'
 
     export default {
         name: 'Home',
@@ -16,6 +20,20 @@
                     .get(`http://${window.location.host}`)
                     .then(res => {
                         alert(res.data)
+                    })
+            },
+            getUser() {
+                axios
+                    .get(`http://${window.location.host}/user`)
+                    .then(res => {
+                        alert(JSON.stringify(res.data))
+                    })
+            },
+            getMenu() {
+                axios
+                    .get(`http://${window.location.host}/menu`)
+                    .then(res => {
+                        alert(JSON.stringify(res.data))
                     })
             }
         }
